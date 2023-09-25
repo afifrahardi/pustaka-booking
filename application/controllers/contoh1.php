33f8b9847463a3ed6de6defb73cc738ea62eaf93
afifrahardi <afifwaspada@gmail.com>
2023-09-25 16:31:41 +0700

    tambah data

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>"
+        echo "<h1>Nama saya Afif Rahardi W
+        Saya tinggal DI daerah Magelang
+        Olahraga yang saya sukai adalah
+        Basketball";
+    }
+}
