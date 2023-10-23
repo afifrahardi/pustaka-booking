3b46578e4616ec5b4cdf3442b5ba18f2db555c13
afifrahardi <afifwaspada@gmail.com>
2023-10-23 11:48:41 +0700

    menambahkan isi

--- application/controllers/Web.php
+++ application/controllers/Web.php
@@ -11,4 +11,31 @@ class Web extends CI_Controller{
         $this->load->view('v_index',$data);
         $this->load->view('v_footer',$data);
     }
+    public function about()
+    {
+    $data['judul'] = "Halaman About";
+    $this->load->view('v_header', $data);
+    $this->load->view('v_about', $data);
+    $this->load->view('v_footer', $data);
+    }
+    public function bukutamu()
+    {
+    $data['judul'] = "Halaman Buku Tamu";
+    $this->load->view('v_header', $data);
+    $this->load->view('v_bukutamu', $data);
+    $this->load->view('v_footer', $data);
+    }
+    public function isibukutamu()
+    {
+    $data['judul'] = "Halaman Buku Tamu";
+    $this->load->view('v_header', $data);
+    $data = [
+    'nama' => $this->input->post('nama'),
+    'email' => $this->input->post('email'),
+    'komentar' => $this->input->post('komentar'),
+    ];
+    $this->load->view('v_isibukutamu', $data);
+    $this->load->view('v_footer', $data);
+    }
 }
+
